3ac9bd50c203f5d15417f058280d56354e5361bc
Will Smith <lol.tihan@gmail.com>
2016-08-08 20:59:56 -0400

    Our refactoring, begins

--- src/Foom.Ecs/System.fsi
+++ src/Foom.Ecs/System.fsi
@@ -23,16 +23,16 @@ and internal IEntitySystem<'Update> =
 /// Construct that is responsible for behaviour.
 type EntitySystem<'Update> = internal EntitySystem of (unit -> IEntitySystem<'Update>)
 
-/// Contains functions that help compose Entity Systems.
-[<RequireQualifiedAccess>]
-module EntitySystem =
+///// Contains functions that help compose Entity Systems.
+//[<RequireQualifiedAccess>]
+//module EntitySystem =
 
-    /// Creates an Entity System that will execute the given lambda lazily on initialization.
-    /// This is useful for expressing Entity Systems that have their own state.
-    val build : (unit -> EntitySystem<'Update>) -> EntitySystem<'Update>
+//    /// Creates an Entity System that will execute the given lambda lazily on initialization.
+//    /// This is useful for expressing Entity Systems that have their own state.
+//    val build : (unit -> EntitySystem<'Update>) -> EntitySystem<'Update>
 
-    /// Takes a list of Entity Systems and merges them into one.
-    val merge : name: string -> EntitySystem<'Update> list -> EntitySystem<'Update>
+//    /// Takes a list of Entity Systems and merges them into one.
+//    val merge : name: string -> EntitySystem<'Update> list -> EntitySystem<'Update>
 
 /// Contains Entity System primitives to build more complex Entity Systems.
 module Systems =
@@ -43,12 +43,12 @@ module Systems =
 
     /// Event Listener system that handles the specified event immediately.
     /// This should only be used in a handful of cases.
-    val eventListener<'Update, 'Event when 'Event :> IEntitySystemEvent and 'Event : not struct> : ('Event -> unit) -> EntitySystem<'Update>
+    //val eventListener<'Update, 'Event when 'Event :> IEntitySystemEvent and 'Event : not struct> : ('Event -> unit) -> EntitySystem<'Update>
 
     /// Event Queue system that handles the specified event by placing it into a queue.
     /// The queue will be processed on update. Thread safe.
     /// Partially applies the given lambda on initialization before the update function gets evaluated.
-    val eventQueue<'Update, 'Event when 'Event :> IEntitySystemEvent and 'Event : not struct> : (EntityManager -> EventManager -> ('Update -> 'Event -> unit)) -> EntitySystem<'Update>
+    //val eventQueue<'Update, 'Event when 'Event :> IEntitySystemEvent and 'Event : not struct> : (EntityManager -> EventManager -> ('Update -> 'Event -> unit)) -> EntitySystem<'Update>
 
     /// Shutdown system that executes the given lambda when a system has been called to shutdown.
-    val shutdown<'Update> : (unit -> unit) -> EntitySystem<'Update>
+    //val shutdown<'Update> : (unit -> unit) -> EntitySystem<'Update>
--- src/Foom/Program.fs
+++ src/Foom/Program.fs
@@ -5,112 +5,17 @@ open System.Numerics
 
 open Foom.Client
 
-// http://gafferongames.com/game-physics/fix-your-timestep/
-module GameLoop =
-    type private GameLoop<'T> = { 
-        State: 'T
-        PreviousState: 'T
-        LastTime: int64
-        UpdateTime: int64
-        UpdateAccumulator: int64
-        RenderAccumulator: int64
-        RenderFrameCount: int
-        RenderFrameCountTime: int64
-        RenderFrameLastCount: int }
-
-    let start (state: 'T) (pre: unit -> unit) (update: int64 -> int64 -> 'T -> 'T) (render: float32 -> 'T -> 'T -> unit) =
-        let targetUpdateInterval = (1000. / 30.) * 10000. |> int64
-        let targetRenderInterval = (1000. / 12000.) * 10000. |> int64
-        let skip = (1000. / 5.) * 10000. |> int64
-
-        let stopwatch = Stopwatch.StartNew ()
-        let inline time () = stopwatch.Elapsed.Ticks
-
-        let rec loop gl =
-            let currentTime = time ()
-            let deltaTime =
-                match currentTime - gl.LastTime with
-                | x when x > skip -> skip
-                | x -> x
-
-            let updateAcc = gl.UpdateAccumulator + deltaTime
-
-            // We do not want our render accumulator going out of control,
-            // so let's put a limit of its interval.
-            let renderAcc = 
-                match gl.RenderAccumulator with
-                | x when x > targetRenderInterval -> targetRenderInterval
-                | x -> x + deltaTime
-
-            let rec processUpdate gl =
-                if gl.UpdateAccumulator >= targetUpdateInterval
-                then
-                    let state = update gl.UpdateTime targetUpdateInterval gl.State
-
-                    processUpdate
-                        { gl with 
-                            State = state
-                            PreviousState = gl.State
-                            UpdateTime = gl.UpdateTime + targetUpdateInterval
-                            UpdateAccumulator = gl.UpdateAccumulator - targetUpdateInterval }
-                else
-                    gl
-
-            let processRender gl =
-                if gl.RenderAccumulator >= targetRenderInterval then
-                    render (single gl.UpdateAccumulator / single targetUpdateInterval) gl.PreviousState gl.State
-
-                    let renderCount, renderCountTime, renderLastCount =
-                        if currentTime >= gl.RenderFrameCountTime + (10000L * 1000L) then
-                            printfn "%A" gl.RenderFrameLastCount
-                            1, gl.RenderFrameCountTime + (10000L * 1000L), gl.RenderFrameCount
-                        else
-                            gl.RenderFrameCount + 1, gl.RenderFrameCountTime, gl.RenderFrameLastCount
-
-                    { gl with 
-                        LastTime = currentTime
-                        RenderAccumulator = gl.RenderAccumulator - targetRenderInterval
-                        RenderFrameCount = renderCount
-                        RenderFrameCountTime = renderCountTime
-                        RenderFrameLastCount = renderLastCount }
-                else
-                    { gl with LastTime = currentTime }
-
-            pre ()
-       
-            { gl with UpdateAccumulator = updateAcc; RenderAccumulator = renderAcc }
-            |> processUpdate
-            |> processRender
-            |> loop
-
-        loop
-            { State = state
-              PreviousState = state
-              LastTime = 0L
-              UpdateTime = 0L
-              UpdateAccumulator = targetUpdateInterval
-              RenderAccumulator = 0L
-              RenderFrameCount = 0
-              RenderFrameCountTime = 0L
-              RenderFrameLastCount = 0 }
-
-open Foom.Client
-
-type GameState = {
-    Client: Client.ClientState }
-
 [<EntryPoint>]
 let main argv = 
     Runtime.GCSettings.LatencyMode <- Runtime.GCLatencyMode.Batch
 
-    GameLoop.start { Client = Client.init () }
-        id
-        (fun time interval curr ->
-            GC.Collect (0)
+    let client = Client.init ()
 
-            curr
+    GameLoop.start 30.
+        (fun time interval ->
+            GC.Collect (0)
         ) 
-        (fun t prev curr ->
-            Client.draw t prev.Client curr.Client
+        (fun t ->
+            Client.draw t client client
         )
     0
