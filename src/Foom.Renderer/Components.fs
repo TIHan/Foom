e948a87682d62376c5950410cc4f91fca5474501
Will Smith <lol.tihan@gmail.com>
2016-09-01 23:20:59 -0400

    Don't need that

--- src/Foom.Renderer/Components.fs
+++ src/Foom.Renderer/Components.fs
@@ -38,50 +38,10 @@ type MeshState =
     | Loaded of Mesh
 
 type MeshComponent (vertices: Vector3 [], uv) =
-        
-    let mutable min = Vector3.Zero
-    let mutable max = Vector3.Zero
-
-    do
-        if vertices.Length > 0 then
-            let firstV = vertices.[0]
-
-            let mutable minX = firstV.X
-            let mutable minY = firstV.Y
-            let mutable minZ = firstV.Z
-            let mutable maxX = firstV.X
-            let mutable maxY = firstV.Y
-            let mutable maxZ = firstV.Z
-
-            vertices
-            |> Array.iter (fun v ->
-                if v.X < minX then
-                    minX <- v.X
-                elif v.X > maxX then
-                   maxX <- v.X
-
-                if v.Y < minY then
-                    minY <- v.Y
-                elif v.Y > maxY then
-                   maxY <- v.Y
-
-                if v.Z < minZ then
-                    minZ <- v.Z
-                elif v.Z > maxZ then
-                   maxZ <- v.Z
-            )
-
-            min <- Vector3 (minX, minY, minZ)
-            max <- Vector3 (maxX, maxY, maxZ)
 
     member val State = MeshState.ReadyToLoad (vertices, uv) with get, set
 
-    member __.Min = min
-
-    member __.Max = max
-
     interface IComponent
-
 
 [<RequireQualifiedAccess>]
 type TextureState =
