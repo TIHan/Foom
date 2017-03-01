a0c2f55225bf4ee65651dca48b0a1c6629dd373a
Will Smith <lol.tihan@gmail.com>
2017-02-28 22:58:36 -0800

    Starting to move renderer stuff into a PCL

--- src/Foom.Renderer.Desktop/Buffers.fs
+++ src/Foom.Renderer.Desktop/Buffers.fs
@@ -6,41 +6,6 @@ open System.Numerics
 open System.Collections.Generic
 open System.Runtime.InteropServices
 
-type IGL =
-
-    abstract BindBuffer : int -> unit
-
-    abstract CreateBuffer : unit -> int
-
-    abstract DeleteBuffer : int -> unit
-
-    abstract BufferData : Vector2 [] * count: int * bufferId: int -> unit
-
-    abstract BufferData : Vector3 [] * count: int * bufferId: int -> unit
-
-    abstract BufferData : Vector4 [] * count: int * bufferId: int -> unit
-
-
-    abstract BindTexture : int -> unit
-
-    abstract CreateTexture : width: int * height: int * data: nativeint -> int
-
-    abstract DeleteTexture : int -> unit
-
-
-    abstract BindFramebuffer : int -> unit
-
-    abstract CreateFramebuffer : unit -> int
-
-    abstract CreateFramebufferTexture : width: int * height: int * data: nativeint -> int
-
-    abstract SetFramebufferTexture : int -> unit
-
-    abstract CreateRenderBuffer : width: int * height: int -> int
-
-
-    abstract Clear : unit -> unit
-
 type DesktopGL () =
 
     interface IGL with
