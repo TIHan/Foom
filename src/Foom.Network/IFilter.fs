5bc0d4609ff01e35563d362fa5cefe1f71378098
TIHan <lol.tihan@gmail.com>
2017-03-25 20:44:12 -0700

    Build out some basic pipeline infrastructure

--- src/Foom.Network/IFilter.fs
+++ src/Foom.Network/IFilter.fs
@@ -3,35 +3,138 @@
 open System
 open System.Collections.Generic
 
-type Source (packetPool : PacketPool, map: byte[] -> int -> int -> Packet -> unit) =
+type ISource =
 
-    let queue = Queue<Packet> ()
+    abstract Send : byte [] * startIndex: int * size: int -> unit
 
-    member this.SendData (data, startIndex, size) =
-        let packet = packetPool.Get ()
+    abstract Listen : IObservable<Packet>
 
-        map data startIndex size packet
+type IPass =
 
-        queue.Enqueue packet
- 
-    member this.Flush f =
-        while queue.Count > 0 do
-            let packet = queue.Dequeue ()
-            f packet
-            packetPool.Recycle packet
+    abstract Send : Packet -> unit
+
+    abstract Listen : IObservable<Packet>
+
+type IQueue =
+    inherit IPass
+
+    abstract Flush : unit -> unit
+
+type ElementContext =
+    {
+        [<DefaultValue>] mutable source: byte [] -> int -> int -> unit
+        packetPool : PacketPool
+        queues : ResizeArray<IQueue>
+        mutable nextListener : IObservable<Packet>
+    }
+
+type Element = Element of (ElementContext -> unit)
+
+type SinkElement = SinkElement of (ElementContext -> unit)
+
+module PipelineTest =
+
+    type UnreliableSource (packetPool : PacketPool) =
+
+        let outputEvent = Event<Packet> ()
+
+        interface ISource with 
+
+            member val Listen : IObservable<Packet> = outputEvent.Publish :> IObservable<Packet>
+
+            member this.Send (data, startIndex, size) =
+                let packet = packetPool.Get ()
+
+                if size > packet.LengthRemaining then
+                    failwith "Unreliable data is larger than what a new packet can hold. Consider using reliable sequenced."
+
+                packet.SetData (PacketType.Unreliable, data, startIndex, size)
+                outputEvent.Trigger packet
+
+    type PacketMerger (packetPool : PacketPool) =
+
+         let listenEvent = Event<Packet> ()
+
+         let packets = ResizeArray<Packet> (packetPool.Amount)
+
+         interface IQueue with
+
+            member val Listen : IObservable<Packet> = listenEvent.Publish :> IObservable<Packet>
+
+            member x.Send (packet : Packet) =
+                if packets.Count > 0 then
+
+                    let mutable done' = false
+                    for i = 0 to packets.Count - 1 do
+                        let packet' = packets.[i]
+                        if packet'.LengthRemaining > packet.Length && not done' then
+                            packet'.Merge packet
+                            done' <- true
+
+                    if not done' then
+                        let packet' = packetPool.Get ()
+                        packet.CopyTo packet'
+                        packets.Add packet'
+                else
+                    let packet' = packetPool.Get ()
+                    packet.CopyTo packet'
+                    packets.Add packet'
 
 
-type SequenceFilter (packetPool : PacketPool) =
+            member x.Flush () =
+                packets
+                |> Seq.iter (fun packet ->
+                    listenEvent.Trigger packet
+                )
+                packets.Clear ()
 
-    let mutable nextSeqId = 0us
+    let create (src: ISource) =
 
-    member this.In (packet : Packet) =
-        match packet.PacketType with
-        | PacketType.UnreliableSequenced
-        | PacketType.Reliable
-        | PacketType.ReliableSequenced
-        | PacketType.ReliableOrdered ->
+        Element (fun context ->
+            context.source <- fun data startIndex size -> src.Send (data, startIndex, size)
+            context.nextListener <- src.Listen
+        )
 
-            ()
+    let addQueue (q : IQueue) el =
+        Element (fun context ->
+            match el with
+            | Element f -> f context
 
-        | _ -> failwith "Can't send packet unreliable packet with a sequence id."
+            if context.nextListener <> null then
+                context.nextListener.Add q.Send
+                context.queues.Add q
+                context.nextListener <- q.Listen
+        )
+
+    let sink f el =
+        SinkElement (fun context ->
+            match el with
+            | Element f -> f context
+
+            if context.nextListener <> null then
+                context.nextListener.Add (fun packet ->
+                    f packet
+                    context.packetPool.Recycle packet
+                )
+                context.nextListener <- null
+        )
+
+    let build (sinkElement: SinkElement) =
+        let context =
+            {
+                packetPool = PacketPool 64
+                queues = ResizeArray ()
+                nextListener = null
+            }
+        match sinkElement with
+        | SinkElement f -> f context
+        context
+
+    let basicPipeline () =
+        let packetPool = PacketPool 64
+        let source = UnreliableSource packetPool
+        let packetQueue = PacketMerger packetPool
+
+        create source
+        |> addQueue packetQueue
+        |> sink (fun packet -> ())
