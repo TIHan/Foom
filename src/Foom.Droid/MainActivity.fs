43cc1a9e351083bccbc55ac149e05b58835b2258
TIHan <lol.tihan@gmail.com>
2017-04-14 23:45:24 -0700

    OpenGL ES3 works on Android

--- src/Foom.Droid/MainActivity.fs
+++ src/Foom.Droid/MainActivity.fs
@@ -15,7 +15,7 @@ open OpenTK.Graphics.ES30
 
 type Resources = Foom.Droid.Resource
 
-type GLRenderer () =
+type GLRenderer (surfaceView: GLSurfaceView) =
     inherit Java.Lang.Object ()
 
     let mutable vaoId = 0
@@ -23,42 +23,42 @@ type GLRenderer () =
     interface GLSurfaceView.IRenderer with
 
         member x.OnDrawFrame _ =
-            ()
+            GL.Clear (ClearBufferMask.ColorBufferBit)
 
         member x.OnSurfaceChanged (gl, width, height) =
             GL.Viewport (0, 0, width, height)
 
         member x.OnSurfaceCreated (gl, config) =
+            GL.ClearColor (Color4.Green)
             GL.GenVertexArrays (1, &vaoId)
             GL.BindVertexArray (vaoId)
 
-type GLView (context) as this =
-    inherit GLSurfaceView (context)
+type GLView (context) as x =
+    inherit RelativeLayout (context)
 
-    let renderer = new GLRenderer ()
+    let surfaceView = new GLSurfaceView (context)
+    let renderer = new GLRenderer (surfaceView)
 
     do
-        this.SetEGLContextClientVersion (3)
-        this.SetEGLConfigChooser (8, 8, 8, 8, 16, 0)
-        this.SetRenderer (renderer)
+        surfaceView.SetEGLContextClientVersion (3)
+        surfaceView.SetEGLConfigChooser (8, 8, 8, 8, 16, 8)
+        surfaceView.SetRenderer (renderer)
+
+        x.AddView surfaceView
 
 [<Activity (Label = "Foom", MainLauncher = true, Icon = "@mipmap/icon")>]
 type MainActivity () =
     inherit Activity ()
 
-    let mutable count:int = 1
-
     override this.OnCreate (bundle) =
 
         base.OnCreate (bundle)
 
-        // Set our view from the "main" layout resource
-        this.SetContentView (Resources.Layout.Main)
+        let glView = new GLView (this :> Context)
 
-        // Get our button from the layout resource, and attach an event to it
-        let button = this.FindViewById<Button>(Resources.Id.myButton)
-        button.Click.Add (fun args -> 
-            button.Text <- sprintf "%d clicks!" count
-            count <- count + 1
-        )
+        let a = new Button (this)
+        a.Text <- "YOOOPAC"
 
+        glView.AddView a
+       
+        this.SetContentView (glView)
