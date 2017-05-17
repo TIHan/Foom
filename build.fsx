8f1e5113a7da4b79068e2996ed14d04d71a1fc75
Will Smith <lol.tihan@gmail.com>
2017-05-17 13:39:04 -0700

    Added FAKE

---
+++ build.fsx
@@ -0,0 +1,11 @@
+// include Fake lib
+#r @"packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+// Default target
+Target "Default" (fun _ ->
+    trace "Hello World from FAKE"
+)
+
+// start build
+RunTargetOrDefault "Default"
