1a68c0592500a5ebc9bba5ed8224cef214be22ec
Will Smith <lol.tihan@gmail.com>
2017-05-18 23:01:29 -0700

    Added Foom.Game.Doom.Entities

---
+++ src/Foom.Game.Level/LevelWorld.fs
@@ -0,0 +1,20 @@
+﻿namespace Foom.Game.Level
+
+open Foom.Ecs
+
+[<Sealed>]
+type LevelWorld (world : World) =
+
+    let updates = ResizeArray ()
+
+    member val Subworld = world.CreateSubworld ()
+
+    member this.AddBehaviors behaviors =
+        behaviors
+        |> Seq.map this.Subworld.AddBehavior
+        |> Seq.iter updates.Add
+
+    member this.Update data =
+        for i = 0 to updates.Count - 1 do
+            let update = updates.[i]
+            update data
