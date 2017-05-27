cb5c2b751aab92d3c181859f26c6ee0a25c7895b
TIHan <lol.tihan@gmail.com>
2017-05-26 22:34:35 -0700

    cleaning up

--- src/Foom.Network/Pipelines.fs
+++ src/Foom.Network/Pipelines.fs
@@ -7,45 +7,6 @@ open System.Collections.Generic
 open Foom.Network
 open Foom.Network.Pipeline
 
-let ReceiverSource (packetPool : PacketPool) =
-
-    { new ISource with 
-
-        member x.Send (data, startIndex, size, output) =
-            let packet = packetPool.Get ()
-            packet.Set (data, startIndex, size)
-            output packet }
-
-let ReliableOrderedAckReceiver (packetPool : PacketPool) (ackManager : AckManager) ack =
-
-    let mutable nextSeqId = 0us
-    let queue = Queue<Packet> ()
-
-    { new IFilter with
-
-        member x.Send packet =
-            if nextSeqId = packet.SequenceId then
-                queue.Enqueue packet
-                nextSeqId <- nextSeqId + 1us
-            else
-                ackManager.MarkCopy packet
-
-        member x.Process output =
-            while queue.Count > 0 do
-                let packet = queue.Dequeue ()
-                ack packet.SequenceId
-                output packet
-
-            ackManager.ForEachPending (fun seqId copyPacket ->
-                if int nextSeqId = seqId then
-                    let packet = packetPool.Get ()
-                    copyPacket.CopyTo packet
-                    ackManager.Ack seqId
-                    ack nextSeqId
-                    nextSeqId <- nextSeqId + 1us
-                    output packet
-            ) }
-
 module NewPipeline =
 
     open NewPipeline
@@ -76,60 +37,6 @@ module NewPipeline =
                     callback packet
             )
         )
-
-let ReliableAckReceiver (packetPool : PacketPool) ack =
-
-    let queue = Queue<Packet> ()
-
-    { new IFilter with
-
-        member x.Send packet =
-            queue.Enqueue packet
-
-        member x.Process output =
-            while queue.Count > 0 do
-                let packet = queue.Dequeue ()
-                ack packet.SequenceId
-                output packet
-    }
-
-let ReliableSequencedAckReceiver (packetPool : PacketPool) ack =
-
-    let mutable recentFrom = 0us
-    let mutable recentTo = 0us
-    let queue = Queue<Packet> ()
-
-    { new IFilter with
-
-        member x.Send packet =
-
-            // A new message has been received.
-            if sequenceMoreRecent packet.SequenceId recentTo then
-
-                while queue.Count > 0 do
-                    packetPool.Recycle (queue.Dequeue ())
-
-                recentFrom <- packet.SequenceId
-                recentTo <- packet.SequenceId + uint16 packet.Fragments
-                queue.Enqueue packet
-
-            // The packet could be a fragment of the entire message.
-            elif sequenceMoreRecent packet.SequenceId recentFrom then
-                if packet.FragmentId > 0us || packet.Fragments > 0uy then
-                    queue.Enqueue packet
-                else
-                    failwith "shouldn't happen yet"
-                    packetPool.Recycle packet
-
-            else
-                packetPool.Recycle packet
-            
-        member x.Process output =
-            while queue.Count > 0 do
-                let packet = queue.Dequeue ()
-                ack packet.SequenceId
-                output packet
-    }
 
 [<Struct>]
 type Data = { bytes : byte []; startIndex : int; size : int }
@@ -182,13 +89,4 @@ let basicReceiver packetPool =
     NewPipeline.createPipeline receiveFilter
     |> NewPipeline.build
 
-let reliableOrderedReceiver ack f =
-    let packetPool = PacketPool 64
-    let ackManager = AckManager ()
 
-    create (ReceiverSource packetPool)
-    |> add (ReliableOrderedAckReceiver packetPool ackManager ack)
-    |> sink (fun packet ->
-        f packet
-        packetPool.Recycle packet
-    )
