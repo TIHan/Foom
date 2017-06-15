a1941574245f4f9d4b1492274517c3e683198ce6
Will Smith <wismith@microsoft.com>
2017-06-14 18:28:56 -0700

    Breaking out some functions

--- src/Foom.Network/AckManager.fs
+++ src/Foom.Network/AckManager.fs
@@ -3,27 +3,72 @@
 open System
 open System.Collections.Generic
 
-//http://gafferongames.com/networking-for-game-programmers/reliability-and-flow-control/
-//bool sequence_more_recent( unsigned int s1, 
-//                           unsigned int s2, 
-//                           unsigned int max )
-//{
-//    return 
-//        ( s1 > s2 ) && 
-//        ( s1 - s2 <= max/2 ) 
-//           ||
-//        ( s2 > s1 ) && 
-//        ( s2 - s1  > max/2 );
-//}
 [<AutoOpen>]
-module AcksInternal =
-
+module AckManagerHelpers =
+    //http://gafferongames.com/networking-for-game-programmers/reliability-and-flow-control/
+    //bool sequence_more_recent( unsigned int s1, 
+    //                           unsigned int s2, 
+    //                           unsigned int max )
+    //{
+    //    return 
+    //        ( s1 > s2 ) && 
+    //        ( s1 - s2 <= max/2 ) 
+    //           ||
+    //        ( s2 > s1 ) && 
+    //        ( s2 - s1  > max/2 );
+    //}
     let sequenceMoreRecent (s1 : uint16) (s2 : uint16) =
         (s1 > s2) &&
         (s1 - s2 <= UInt16.MaxValue / 2us)
             ||
         (s2 > s1) &&
         (s2 - s1 > UInt16.MaxValue / 2us)
+
+    let iterPending (oldestAck : int) (newestAck : int) (acks : bool []) f =
+        if oldestAck <> -1 then
+
+            if newestAck = oldestAck then
+                if not acks.[oldestAck] then
+                    f oldestAck
+            
+            elif newestAck > oldestAck then
+                for i = oldestAck to newestAck do
+                    if not acks.[i] then
+                        f i
+
+            elif newestAck < oldestAck then
+                for i = oldestAck to acks.Length - 1 do
+                    if not acks.[i] then
+                        f i
+
+                for i = 0 to newestAck do
+                    if not acks.[i] then
+                        f i
+
+    let getOldestAndNewestAck i (oldestAck : int) (newestAck : int) (acks : bool []) =
+        if oldestAck = newestAck && oldestAck = i then
+            struct (-1, -1)
+
+        elif oldestAck = i then
+            let mutable n = uint16 oldestAck
+            let mutable nextOldestAck = -1
+            while nextOldestAck = -1 do
+                n <- n + 1us
+                if not acks.[int n] then
+                    nextOldestAck <- int n
+            struct (nextOldestAck, newestAck)
+
+        elif newestAck = i then
+            let mutable n = uint16 oldestAck
+            let mutable nextNewestAck = -1
+            while nextNewestAck = -1 do
+                n <- n - 1us
+                if not acks.[int n] then
+                    nextNewestAck <- int n
+            struct (oldestAck, nextNewestAck)
+
+        else
+            struct (oldestAck, newestAck)
 
 [<Sealed>]
 type AckManager (ackRetryTime : TimeSpan) =
@@ -36,31 +81,12 @@ type AckManager (ackRetryTime : TimeSpan) =
     let mutable newestAck = -1
     let mutable oldestAck = -1
 
-    member x.ForEachPending time f1 =
-        let inline f i packet =
+    member x.ForEachPending time f =
+        iterPending oldestAck newestAck acks (fun i -> 
             if time > ackTimes.[i] + ackRetryTime then
                 ackTimes.[i] <- ackTimes.[i] + ackRetryTime
-                f1 i packet
-
-        if oldestAck <> -1 then
-
-            if newestAck = oldestAck then
-                if not acks.[oldestAck] then
-                    f oldestAck copyPackets.[oldestAck]
-            
-            elif newestAck > oldestAck then
-                for i = oldestAck to newestAck do
-                    if not acks.[i] then
-                        f i copyPackets.[i]
-
-            elif newestAck < oldestAck then
-                for i = oldestAck to acks.Length - 1 do
-                    if not acks.[i] then
-                        f i copyPackets.[i]
-
-                for i = 0 to newestAck do
-                    if not acks.[i] then
-                        f i copyPackets.[i]
+                f i copyPackets.[i]
+        )
 
     member x.Ack i =
         if not acks.[i] then
@@ -70,27 +96,9 @@ type AckManager (ackRetryTime : TimeSpan) =
             acks.[i] <- true
             ackTimes.[i] <- TimeSpan.Zero
 
-            if oldestAck = newestAck && oldestAck = i then
-                oldestAck <- -1
-                newestAck <- -1
-
-            elif oldestAck = i then
-                let mutable n = uint16 oldestAck
-                let mutable nextOldestAck = -1
-                while nextOldestAck = -1 do
-                    n <- n + 1us
-                    if not acks.[int n] then
-                        nextOldestAck <- int n
-                oldestAck <- nextOldestAck
-
-            elif newestAck = i then
-                let mutable n = uint16 oldestAck
-                let mutable nextNewestAck = -1
-                while nextNewestAck = -1 do
-                    n <- n - 1us
-                    if not acks.[int n] then
-                        nextNewestAck <- int n
-                newestAck <- nextNewestAck
+            let struct (newOldestAck, newNewestAck) = getOldestAndNewestAck i oldestAck newestAck acks
+            oldestAck <- newOldestAck
+            newestAck <- newNewestAck
 
     member x.MarkCopy (packet : Packet, time) =
         let i = int packet.SequenceId
